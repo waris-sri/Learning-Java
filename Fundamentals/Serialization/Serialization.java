24ecc83c4dd27e5322ac67672aa2ad853e5c5571
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-04-22 15:27:09 +0700

    added Serialization

---
+++ Fundamentals/Serialization/Serialization.java
@@ -0,0 +1,29 @@
+/* SERIALIZATION
+ * - converts an object into a byte stream (.ser) file
+ *   - .ser is platform independent
+ * - saves the current state of an object after the program exists
+ * - the file can be saved locally, to another machine, or stored in a network
+ * - basically saving a file with the object's information
+ * 
+ * DESERIALIZATION
+ * - reversed process of serialization; converts a byte stream into an object
+ * - basically loading a saved file
+ */
+
+public class Serialization {
+    
+    public class User {
+        String name;
+        String password;
+        public void greet() {
+            System.out.println("Hello, " + name + "!");
+        }
+    }
+    
+    public static void main(String[] args) {
+        User user = new User();
+        user.name = "John";
+        user.password = "12345";
+        user.greet();
+    }
+}
