7856de3ab0e74bbb439f11435aea4680a0621253
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-05-21 05:23:53 +0700

    updated Solution.java

--- Solution.java
+++ Solution.java
@@ -1,21 +1,22 @@
-/* HackerRank Practice Problems */
-// Java Loops II
-import java.util.Scanner;
-public class Solution {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int q = 1;
-        for (int j = 0; j < q; j++) {
-            int a = 0;
-            int b = 2;
-            int n = 10;
-            int result = a;
-            for (int i = 0; i < n; i++) {
-                result += Math.pow(2, i) * b;
-                System.out.print(result + " ");
-            }
-            System.out.println();
+/* 
+ * Keys:
+ * - check palindrome by comparing the first half and the later half of the same integer (halve the number))
+ * - we can get the last digit of an integer by %10
+ * - we can get rid of the last digit of an integer by /10
+ */
+class Solution {
+    public boolean isPalindrome(int x) {
+        // Base Cases
+        if (x == 0) return true;
+        if (x < 0 || x % 10 == 0) return false; // `x%10 == 0` is for leading zeroes in reversed integer
+        // Palindrome Check
+        int rev = 0;
+        while (x > rev) {
+            int lastDigit = x % 10;
+            x /= 10; // remove the last digit
+            rev = (rev * 10) + lastDigit;
         }
-        scanner.close();
+        if (x == rev || x == rev / 10) return true; // `rev / 10` is to address the extra 1 digit of reversed number
+        else return false;
     }
 }
