63d15a449a16d9c6ad247672da3b2ffd6053b634
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-06-23 21:06:25 +0700

    Added GroupAnagrams.java in Coding Problems

---
+++ Solution.java
@@ -0,0 +1,49 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Solution {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+
+        HashMap<String, List<String>> freqMap = new HashMap<>();
+
+        for (String word : strs) {
+            String charFreq = getCharFreq(word);
+            if (freqMap.containsKey(charFreq)) {
+                freqMap.get(charFreq).add(word);
+            } else {
+                List<String> wordList = new ArrayList<>();
+                wordList.add(word);
+                freqMap.put(charFreq, wordList);
+            }
+        }
+
+        return new ArrayList<>(freqMap.values());
+    }
+
+    public static String getCharFreq(String word) {
+        int[] count = new int[26];
+
+        for (char letter : word.toCharArray()) {
+            count[letter - 'a']++;
+        }
+
+        StringBuilder charFreq = new StringBuilder("");
+        char charList = 'a'; // start from 'a' disregarding what letters there are
+        for (int charIndexInWord : count) {
+            charFreq.append(charIndexInWord);
+            charFreq.append(charList); // ex. "better" = "1b2e2t1r" (as HashMap key)
+            charIndexInWord++;
+        }
+        
+        return charFreq.toString();
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+}
--- Solutions.java
+++
@@ -1,7 +0,0 @@
-import java.lang.Math;
-
-class Solutions {
-    public static String longestPalindrome(String input) {
-        // Sample string!
-    }
-}
