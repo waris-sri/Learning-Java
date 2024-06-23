345eef69d15aa5e14cfe582dfe9d9b80e001b3d5
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-06-23 23:17:45 +0700

    Refactored GroupAnagrams.java

--- Algorithms/Coding Problems/GroupAnagrams.java
+++ Algorithms/Coding Problems/GroupAnagrams.java
@@ -10,24 +10,19 @@ public class GroupAnagrams {
         if (words == null || words.length == 0) {
             return new ArrayList<>();
         }
-
         HashMap<String, List<String>> anagramMap = new HashMap<>();
-
         for (String word : words) {
             String anagramKey = getAnagramKey(word);
             anagramMap.computeIfAbsent(anagramKey, key -> new ArrayList<>()).add(word);
         }
-
         return new ArrayList<>(anagramMap.values());
     }
 
     private static String getAnagramKey(String word) {
         int[] count = new int[26];
-
         for (char letter : word.toCharArray()) {
             count[letter - 'a']++;
         }
-
         StringBuilder key = new StringBuilder();
         for (int i = 0; i < count.length; i++) {
             if (count[i] != 0) { // exclude letters with 0 frequency from keys
@@ -35,7 +30,6 @@ public class GroupAnagrams {
                 key.append('a' + i); // ex. "better" = "1b2e2t1r" (as HashMap key)
             }
         }
-
         return key.toString();
     }
 
