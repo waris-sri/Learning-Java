fff9fd38e6b4a4e70c1ff38d579250f46f2583e6
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-10-08 18:42:21 +0700

    +3

--- OOP/Classroom/Students.java
+++ OOP/Classroom/Students.java
@@ -1,66 +1,77 @@
 // (must create a new method outside the Main one; yet, not necessary to do so in a separate file)
 
 public class Students {
-    public static Object others;
-    String name;
-    String school;
-    String university;
-    int age;
-    float height;
 
-    Students(String name, String school, String university, int age, float height) {
-        this.name = name;
-        this.school = school;
-        this.university = university;
-        this.age = age;
-        this.height = height;
-    }
+  public static Object others;
+  String name;
+  String school;
+  String university;
+  int age;
+  float height;
 
-    void learn(String name) {
-        System.out.println(this.name + " is learning…");
-    }
+  Students(
+    String name,
+    String school,
+    String university,
+    int age,
+    float height
+  ) {
+    this.name = name;
+    this.school = school;
+    this.university = university;
+    this.age = age;
+    this.height = height;
+  }
 
-    void rest(String name) {
-        System.out.println(this.name + " is taking a rest…");
-    }
+  void learn(String name) {
+    System.out.println(this.name + " is learning…");
+  }
 
-    void attend(String name) {
-        System.out.printf("%24s: Present ✅%n", this.name);
-    }
+  void rest(String name) {
+    System.out.println(this.name + " is taking a rest…");
+  }
 
-    void absent(String name) {
-        System.out.printf("%24s: Absent ❌%n", this.name);
-    }
+  void attend(String name) {
+    System.out.printf("%24s: Present ✅%n", this.name);
+  }
+
+  void absent(String name) {
+    System.out.printf("%24s: Absent ❌%n", this.name);
+  }
 }
 
 class Beam {
-    String name = "Waris Sripatoomrak";
-    String school = "Saint Gabriel's College";
-    String university = "Mahidol University";
-    int age = 18;
-    float height = 1.78f;
+
+  String name = "Waris Sripatoomrak";
+  String school = "Saint Gabriel's College";
+  String university = "Mahidol University";
+  int age = 18;
+  float height = 1.78f;
 }
 
 class Japan {
-    String name = "Thanakrit Sriphiphattana";
-    String school = "Saint Gabriel's College";
-    String university = "Chulalongkorn University";
-    int age = 18;
-    float height = 1.77f;
+
+  String name = "Thanakrit Sriphiphattana";
+  String school = "Saint Gabriel's College";
+  String university = "King Mongkut's Institute of Technology Ladkrabang";
+  int age = 18;
+  float height = 1.77f;
 }
 
 class Nay {
-    String name = "Phonlaphat Sitthadetwongggg";
-    String school = "Saint Gabriel's College";
-    String university = "Mahidol University";
-    int age = 17;
-    float height = 1.65f;
+
+  String name = "Phonlaphat Sitthadetwongggg";
+  String school = "Saint Gabriel's College";
+  String university = "Kasetsart University";
+  int age = 17;
+  float height = 1.65f;
 }
 
 class Aie {
-    String name = "Thanet Reantongcome";
-    String school = "Saint Gabriel's College";
-    String university = "Chulalongkorn University";
-    int age = 18;
-    float height = 1.78f;
+
+  String name = "Thanet Reantongcome";
+  String school = "Saint Gabriel's College";
+  String university = "Phramongkutklao College of Medicine";
+  int age = 18;
+  float height = 1.78f;
 }
--- REST API/demo/src/main/java/audiotranscriber/App.java
+++ REST API/demo/src/main/java/audiotranscriber/App.java
@@ -9,62 +9,76 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        // create `transcript` object from the `Transcript` class and set `audio_url`
-        Transcript transcript = new Transcript();
-        transcript.setAudio_url("https://raw.githubusercontent.com/johnmarty3/JavaAPITutorial/main/Thirsty.mp4");
 
-        // use GSON to create a JSON request
-        Gson gson = new Gson();
-        String jsonRequest = gson.toJson(transcript);
-        System.out.println(jsonRequest);
+  public static void main(String[] args) throws Exception {
+    // create `transcript` object from the `Transcript` class and set `audio_url`
+    Transcript transcript = new Transcript();
+    transcript.setAudio_url(
+      "https://raw.githubusercontent.com/johnmarty3/JavaAPITutorial/main/Thirsty.mp4"
+    );
 
-        // create a POST request with `audio_url` as the body of the request
-        HttpRequest postRequest = HttpRequest.newBuilder()
-                // set the base API link
-                .uri(new URI("https://api.assemblyai.com/v2/transcript"))
-                // set the "Authorization" header to given API key
-                .header("Authorization", "b44fb4fe28854b45b347c27d3aa6c64f")
-                // state the HTTP method and input the body (parameter)
-                .POST(BodyPublishers.ofString(jsonRequest))
-                // finish the request
-                .build();
+    // use GSON to create a JSON request
+    Gson gson = new Gson();
+    String jsonRequest = gson.toJson(transcript);
+    System.out.println(jsonRequest);
 
-        // send the created POST request with `HttpClient` (response handler)
-        HttpClient httpClient = HttpClient.newHttpClient();
-        HttpResponse<String> postResponse = httpClient.send(postRequest, BodyHandlers.ofString());
-        System.out.println(postResponse.body());
+    // create a POST request with `audio_url` as the body of the request
+    HttpRequest postRequest = HttpRequest.newBuilder()
+      // set the base API link
+      .uri(new URI("https://api.assemblyai.com/v2/transcript"))
+      // set the "Authorization" header to given API key
+      .header("Authorization", "b44fb4fe28854b45b347c27d3aa6c64f")
+      // state the HTTP method and input the body (parameter)
+      .POST(BodyPublishers.ofString(jsonRequest))
+      // finish the request
+      .build();
 
-        // get the completed `Transcript` object from the JSON response
-        transcript = gson.fromJson(postResponse.body(), Transcript.class);
-        transcript.getId();
+    // send the created POST request with `HttpClient` (response handler)
+    HttpClient httpClient = HttpClient.newHttpClient();
+    HttpResponse<String> postResponse = httpClient.send(
+      postRequest,
+      BodyHandlers.ofString()
+    );
+    System.out.println(postResponse.body());
 
-        // create a GET request with `audio_url` as the body of the request
-        HttpRequest getRequest = HttpRequest.newBuilder()
-                // set the base API link
-                .uri(new URI("https://api.assemblyai.com/v2/transcript" + transcript.getId()))
-                // set the "Authorization" header to given API key and use GET method (default)
-                .header("Authorization", "b44fb4fe28854b45b347c27d3aa6c64f")
-                // finish the request
-                .build();
+    // get the completed `Transcript` object from the JSON response
+    transcript = gson.fromJson(postResponse.body(), Transcript.class);
+    transcript.getId();
 
-        // keep sending the GET request until it is ready or an error occurs
-        while (true) {
-            // send the created GET request
-            HttpResponse<String> getResponse = httpClient.send(getRequest, BodyHandlers.ofString());
-            transcript = gson.fromJson(getResponse.body(), Transcript.class);
-            // print the current status
-            System.out.println(transcript.getStatus());
-            // if the status is "completed" or "error," break off the loop
-            if ("completed".equals(transcript.getStatus()) || "error".equals(transcript.getStatus())) {
-                break;
-            }
-            // wait for 1s then retry
-            Thread.sleep(1000);
-        }
+    // create a GET request with `audio_url` as the body of the request
+    HttpRequest getRequest = HttpRequest.newBuilder()
+      // set the base API link
+      .uri(
+        new URI("https://api.assemblyai.com/v2/transcript" + transcript.getId())
+      )
+      // set the "Authorization" header to given API key and use GET method (default)
+      .header("Authorization", "b44fb4fe28854b45b347c27d3aa6c64f")
+      // finish the request
+      .build();
 
-        // when completed transcribing
-        System.out.println("Transcription Complete!");
-        System.out.println(transcript.getText());
+    // keep sending the GET request until it is ready or an error occurs
+    while (true) {
+      // send the created GET request
+      HttpResponse<String> getResponse = httpClient.send(
+        getRequest,
+        BodyHandlers.ofString()
+      );
+      transcript = gson.fromJson(getResponse.body(), Transcript.class);
+      // print the current status
+      System.out.println(transcript.getStatus());
+      // if the status is "completed" or "error," break off the loop
+      if (
+        "completed".equals(transcript.getStatus()) ||
+        "error".equals(transcript.getStatus())
+      ) {
+        break;
+      }
+      // wait for 1s then retry
+      Thread.sleep(1000);
     }
+
+    // when completed transcribing
+    System.out.println("Transcription Complete!");
+    System.out.println(transcript.getText());
+  }
 }
